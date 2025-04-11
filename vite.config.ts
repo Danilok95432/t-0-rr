a8d19fd6f9a5515e0a8a6ca3eb6bc2aa82e9d7c9
VitMach <machihin_vitalii@mail.ru>
2025-04-11 13:16:20 +0300

    работа с actionsMenu

--- vite.config.ts
+++ vite.config.ts
@@ -29,32 +29,4 @@ export default defineConfig({
 			'Access-Control-Allow-Origin': '*',
 		},
 	},
-
-	build: {
-		// Включение Code Splitting
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					// Отдельные чанки для вендорных библиотек
-					vendor: ['react', 'react-dom', 'react-router-dom'],
-					// Отдельные чанки для страниц
-					operations: ['./src/pages/Operations'],
-					organizations: ['./src/pages/Organizations', './src/pages/Organization'],
-					counterparties: ['./src/pages/Counterparties', './src/pages/Counterparty'],
-					accounts: ['./src/pages/Accounts', './src/pages/Account'],
-					articles: ['./src/pages/Articles', './src/pages/Article'],
-					cases: ['./src/pages/Cases', './src/pages/Case'],
-					transactions: ['./src/pages/Transactions', './src/pages/Transaction'],
-					imports: ['./src/pages/Imports', './src/pages/Import'],
-					summary: [
-						'./src/pages/SummaryCashFlow',
-						'./src/pages/ArticlesAndOrganizations',
-						'./src/pages/CasesAndDeals',
-					],
-				},
-			},
-		},
-		// source maps для production для облегчения отладки
-		sourcemap: true,
-	},
 })
