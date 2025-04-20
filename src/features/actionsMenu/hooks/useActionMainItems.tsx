0df8d5a94e77ac60997e7f1f6ca0334832ca1261
VitMach <machihin_vitalii@mail.ru>
2025-04-21 00:36:43 +0300

    Правка хука useModal

--- src/features/actionsMenu/hooks/useActionMainItems.tsx
+++ src/features/actionsMenu/hooks/useActionMainItems.tsx
@@ -2,71 +2,81 @@ import { IActionItem } from '../types'
 import { useEditingMode } from '@/features/editingMode/hooks/useEditingMode'
 import { useFiltersMenu } from '@/features/filtersMenu/hooks/useFiltersMenu'
 import { useModal } from '@/features/modal/hooks/useModal'
+import { useMemo } from 'react'
 
 export const useActionMainItems = (): IActionItem[] => {
-	const { isOpenFiltersMenu, handleOpenFilterMenu } = useFiltersMenu()
-	const { handleOpenModal } = useModal()
-	const { isEditingModeActive, handleActiveEditingMode } = useEditingMode()
+  const { isOpenFiltersMenu, handleOpenFilterMenu } = useFiltersMenu()
+  const { handleOpenModal } = useModal()
+  const { isEditingModeActive, handleActiveEditingMode } = useEditingMode()
 
-	return [
-		{
-			id: 'editing',
-			onClick: handleActiveEditingMode,
-			isActive: isEditingModeActive,
-			showOnPaths: ['organization', 'counterparty', 'account', 'article', 'case', 'transaction'],
-		},
-		{
-			id: 'add',
-			onClick: handleOpenModal,
-		},
-		{
-			id: 'search',
-			onClick: handleOpenFilterMenu,
-			isActive: isOpenFiltersMenu,
-			showOnPaths: ['operations', 'summary'],
-		},
-		{
-			id: 'import',
-			onClick: handleOpenModal,
-			showOnPaths: [
-				'operations',
-				'accounts',
-				'articles',
-				'cases',
-				'transactions',
-				'imports',
-				'import',
-				'summary',
-			],
-		},
-		{
-			id: 'unload',
-			onClick: handleOpenModal,
-			showOnPaths: [
-				'operations',
-				'organizations',
-				'counterparties',
-				'accounts',
-				'articles',
-				'cases',
-				'transactions',
-				'imports',
-				'import',
-				'summary',
-			],
-		},
-		{
-			id: 'graph',
-			showOnPaths: [
-				'operations',
-				'accounts',
-				'articles',
-				'cases',
-				'transactions',
-				'imports',
-				'import',
-				'summary',
-			],
-		},
-	]
+  return useMemo(
+    () => [
+      {
+        id: 'editing',
+        onClick: handleActiveEditingMode,
+        isActive: isEditingModeActive,
+        showOnPaths: ['organization', 'counterparty', 'account', 'article', 'case', 'transaction'],
+      },
+      {
+        id: 'add',
+        onClick: handleOpenModal,
+      },
+      {
+        id: 'search',
+        onClick: handleOpenFilterMenu,
+        isActive: isOpenFiltersMenu,
+        showOnPaths: ['operations', 'summary'],
+      },
+      {
+        id: 'import',
+        onClick: handleOpenModal,
+        showOnPaths: [
+          'operations',
+          'accounts',
+          'articles',
+          'cases',
+          'transactions',
+          'imports',
+          'import',
+          'summary',
+        ],
+      },
+      {
+        id: 'unload',
+        onClick: handleOpenModal,
+        showOnPaths: [
+          'operations',
+          'organizations',
+          'counterparties',
+          'accounts',
+          'articles',
+          'cases',
+          'transactions',
+          'imports',
+          'import',
+          'summary',
+        ],
+      },
+      {
+        id: 'graph',
+        showOnPaths: [
+          'operations',
+          'accounts',
+          'articles',
+          'cases',
+          'transactions',
+          'imports',
+          'import',
+          'summary',
+        ],
+      },
+    ],
+    [
+      handleActiveEditingMode,
+      handleOpenFilterMenu,
+      handleOpenModal,
+      isEditingModeActive,
+      isOpenFiltersMenu,
+    ]
+  )
 }
--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react'
+import { FC } from 'react'
 import { motion } from 'motion/react'
 import classNames from 'classnames'
 
@@ -11,64 +11,51 @@ import { Button } from '@/shared/ui/Button'
 import styles from './modal.module.scss'
 
 export const Modal: FC<IModalProps> = ({ title, children, className }) => {
-	const { isOpenModal, handleCloseModal } = useModal()
+  const { isOpenModal, handleCloseModal } = useModal()
 
-	// Обработчик Esc
-	useEffect(() => {
-		if (isOpenModal) {
-			const closeByEscape = (event: KeyboardEvent) => {
-				if (event.key === 'Escape') {
-					handleCloseModal()
-				}
-			}
-			document.addEventListener('keydown', closeByEscape)
-			return () => document.removeEventListener('keydown', closeByEscape)
-		}
-	}, [isOpenModal, handleCloseModal])
+  // обЪект свойств поведения модального окна
+  const fadeIn = {
+    hidden: {
+      opacity: 0,
+      scale: 0.95,
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+    },
+    exit: {
+      opacity: 0,
+      scale: 0.95,
+    },
+  }
 
-	// обЪект свойств поведения модального окна
-	const fadeIn = {
-		hidden: {
-			opacity: 0,
-			scale: 0.95,
-		},
-		visible: {
-			opacity: 1,
-			scale: 1,
-		},
-		exit: {
-			opacity: 0,
-			scale: 0.95,
-		},
-	}
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className={classNames(styles.overlay, { [styles.visible]: isOpenModal })}
+    >
+      <motion.div
+        role='dialog'
+        variants={fadeIn}
+        initial='hidden'
+        animate='visible'
+        exit='exit'
+        className={classNames(styles.content, className)}
+      >
+        <h2 className={styles.title}>{title}</h2>
 
-	return (
-		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-			className={classNames(styles.overlay, { [styles.visible]: isOpenModal })}
-		>
-			<motion.div
-				role='dialog'
-				variants={fadeIn}
-				initial='hidden'
-				animate='visible'
-				exit='exit'
-				className={classNames(styles.content, className)}
-			>
-				<h2 className={styles.title}>{title}</h2>
+        {children}
 
-				{children}
-
-				<Button
-					type='submit'
-					mode='clear'
-					icon={<Icon iconId='close' width='24px' height='24px' />}
-					className={styles.modal_close}
-					onClick={handleCloseModal}
-				/>
-			</motion.div>
-		</motion.div>
-	)
+        <Button
+          type='submit'
+          mode='clear'
+          icon={<Icon iconId='close' width='24px' height='24px' />}
+          className={styles.modal_close}
+          onClick={handleCloseModal}
+        />
+      </motion.div>
+    </motion.div>
+  )
 }
